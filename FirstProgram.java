9852bf1c82f0996e9d32468ffb44ca46e5712177
Roberto Ventura <rventura1@njcu.ed>
2020-09-25 03:53:16 +0000

    this is my third commit

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,5 @@
 public class FirstProgram{
 	public static void main(String[] args){
-	System.out.println("My favorite programming language is linux");
+		System.out.println("I am taking CS407 because I want to learn how Linux work");
 	}
 }
