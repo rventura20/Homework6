50df07c2694a5450c63a578092b01f73426d2017
Roberto Ventura <rventura1@njcu.ed>
2020-09-25 03:59:58 +0000

    this is my forth commit

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,5 @@
 public class FirstProgram{
 	public static void main(String[] args){
-		System.out.println("I am taking CS407 because I want to learn how Linux work");
+		System.out.println("My favorite food is pizza");
 	}
 }
